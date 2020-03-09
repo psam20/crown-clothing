7fffc54d678eecc64ba7a1360c47b0dd79a9bf47
Kishan Gupta <unique.kishan.gupta@gmail.com>
2020-03-09 23:48:18 +0530

    Finished sign in handle method

--- crown-clothing-app/src/Components/sign-in/signin.component.jsx
+++ crown-clothing-app/src/Components/sign-in/signin.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './signin.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth ,signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component {
 
@@ -15,10 +15,17 @@ class SignIn extends React.Component {
       password: ''
     }
   }
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-
-    this.setState({ email: '', password: '' });
+    const {email,password} =this.state;
+    try {
+     await auth.signInWithEmailAndPassword(email,password);
+     this.setState({ email: '', password: '' });
+    }
+    catch(err){
+      console.log(err);
+    }
+    
   }
   handleChange = e => {
     //   our e.target will be input value itself
