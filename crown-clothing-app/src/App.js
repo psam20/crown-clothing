e0c8551ff134c8c1464313063c9cd25a6ab49c8a
Kishan Gupta <unique.kishan.gupta@gmail.com>
2020-03-09 15:32:18 +0530

    adding Header Component and links into the HompePage

--- crown-clothing-app/src/App.js
+++ crown-clothing-app/src/App.js
@@ -5,11 +5,13 @@ import ShopPage from './pages/Shop/shop.component';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import Header from './Components/header-component/header.component';
+
 
 function App() {
   return (
     <div className="App">
-
+       <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route  exact path='/shop' component={ShopPage}/>
